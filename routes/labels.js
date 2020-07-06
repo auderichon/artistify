6a715def081aa8415005b364d467eb4d62c45071
Aude Richon <auderichon@gmail.com>
2020-07-06 11:35:47 +0200

    label and style CRUD

--- routes/labels.js
+++ routes/labels.js
@@ -10,7 +10,7 @@ const labelModel = require("../models/Label");
 
 router.get("/admin", (req, res, next) => {
   labelModel
-    .find() // retreive all the documents in the labels collection
+    .find() // retrieve all the documents in the labels collection
     .then(dbResults =>
       res.render("tables/labels", {
         labels: dbResults
@@ -21,6 +21,43 @@ router.get("/admin", (req, res, next) => {
 
 router.get("/create", (req, res) => {
   res.render("forms/label");
+});
+
+router.post("/create", (req, res) => {
+  const newLabel = req.body;
+  if (req.body.logo === "") newLabel.logo = undefined;
+
+  labelModel.create(newLabel)
+    .then(() => res.redirect("/labels/admin"))
+    .catch((dbErr) => res.send(JSON.stringify(dbErr)));
+});
+
+router.get("/delete/:id", (req, res, next) => {
+  labelModel
+  .findByIdAndDelete(req.params.id)
+  .then(dbRes => {
+    req.flash("success", "label successfully deleted");
+    res.redirect("/labels/admin");
+  })
+  .catch(next);
+});
+
+router.get("/update/:id", async (req, res) => {
+  const label = await labelModel.findById(req.params.id);
+  res.render("forms/label-update.hbs", label);
+});
+
+router.post("/update/:id", async (req, res) => {
+  try {
+    await labelModel.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+    );
+
+    res.redirect("/labels/admin");
+  } catch (err) {
+    res.json(err);
+  }
 });
 
 module.exports = router;
--- routes/styles.js
+++ routes/styles.js
@@ -9,7 +9,7 @@ const styleModel = require("./../models/Style");
 // handle and render the initial view
 router.get("/admin", (req, res, next) => {
   styleModel
-    .find() // retreive all the documents in the labels collection
+    .find() // retrieve all the documents in the styles collection
     .then(dbResults =>
       res.render("tables/styles", {
         styles: dbResults
@@ -20,6 +20,40 @@ router.get("/admin", (req, res, next) => {
 
 router.get("/create", (req, res) => {
   res.render("forms/style");
+});
+
+router.post("/create", (req, res) => {
+  styleModel.create(req.body)
+    .then(() => res.redirect("/styles/admin"))
+    .catch((dbErr) => res.send(JSON.stringify(dbErr)));
+});
+
+router.get("/delete/:id", (req, res, next) => {
+  styleModel
+  .findByIdAndDelete(req.params.id)
+  .then(dbRes => {
+    req.flash("success", "style successfully deleted");
+    res.redirect("/styles/admin");
+  })
+  .catch(next);
+});
+
+router.get("/update/:id", async (req, res) => {
+  const style = await styleModel.findById(req.params.id);
+  res.render("forms/style-update.hbs", style);
+});
+
+router.post("/update/:id", async (req, res) => {
+  try {
+    await styleModel.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+    );
+
+    res.redirect("/styles/admin");
+  } catch (err) {
+    res.json(err);
+  }
 });
 
 module.exports = router;
